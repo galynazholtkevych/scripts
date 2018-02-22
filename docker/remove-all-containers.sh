c1743ef5fa937e52e962af583e1a1d089f51649f
Galyna Zholtkevych <galynazholtkevych1991@gmail.com>
2018-02-22 12:14:08 +0200

    Ащгтв иуееук сщььфтв

--- docker/remove-all-containers.sh
+++ docker/remove-all-containers.sh
@@ -1,8 +1,5 @@
 # /usr/bin/bash
 
-# TODO: to be improved: NAMES head goes to containers_names
-containers_names=$(docker ps --all | egrep "$NAMES"'[^-]' | awk '{ print $NF }')
-echo "Containers" $containers_names
-docker stop $containers_names
-docker rm $containers_names
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
 
